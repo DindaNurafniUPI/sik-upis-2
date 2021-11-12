eaac44aa3108b543656dea3c4534043410d7698b
Dinda Nurafni <90329718+DindaNurafniUPI@users.noreply.github.com>
2021-11-12 16:36:59 +0700

    Add files via upload

---
+++ post10.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>SIK UPIS</title>
+</head>
+<body>
+    
+<body data-new-gr-c-s-check-loaded="14.1036.0" data-gr-ext-installed="" action="post.php" method="post">
+    
+<header>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<div class="w3-container w3-black">
+<img src="https://psikologi.upi.edu/wp-content/uploads/2021/06/Untitled-1-01-e1624637650473.png" class="bi me-2" width="300" height="90" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+    <a href="index.php" class="w3-bar-item w3-button w3-round w3-white ">Home</a>
+    <div class="w3-dropdown-hover">
+      <button class="w3-button  ">Profil</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="post2.php" class="w3-bar-item w3-button">Sejarah</a>
+        <a href="post3.php" class="w3-bar-item w3-button">Visi Misi</a>
+        <a href="post4.php" class="w3-bar-item w3-button">Struktur Organisasi</a>
+        <a href="post1.php" class="w3-bar-item w3-button">Dosen</a>
+      </div>
+    </div>
+    <div class="w3-dropdown-hover">
+      <button class="w3-button  ">Akademik</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="post5.php" class="w3-bar-item w3-button">kurikulum</a>
+        <a href="post6.php" class="w3-bar-item w3-button">Laboratorium</a>
+      </div>
+    </div>
+    <a href="post7.php" class="w3-bar-item w3-button">Akreditasi</a>
+    <div class="w3-dropdown-hover">
+      <button class="w3-button ">Download</button>
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="https://drive.google.com/file/d/1QzVIjXXNbZxCYD2rnOdaMWR_7k83amyw/view" class="w3-bar-item w3-button">SK Syarat Peringat Akreditasi</a>
+        <a href="https://drive.google.com/file/d/1CijlD1g5NZH2Jkwvf90TXoUYX3rdcYp9/view" class="w3-bar-item w3-button">Struktur Kurikulum</a>
+        <a href="https://drive.google.com/drive/folders/12tzM1qZQ9NG99olZvmA_syL-TH44oXh2" class="w3-bar-item w3-button">Rencana pembelajaran studi (RPS)</a>
+        <a href="https://drive.google.com/drive/folders/1ZlC8ZQWGvVBEEoRyfmFhJ83XofXxtgrY?usp=sharing" class="w3-bar-item w3-button">Nilai Perkuliahan</a>
+      </div>
+    </div>
+    <a href="post8.php" class="w3-bar-item w3-button ">FAQ</a>
+    <a href="post9.php" class="w3-bar-item w3-button ">HIMATASKA</a><br>
+</header>
+
+
+<center>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body action="post.php" method="post">
+<img src="sik.jpg" alt="Chicago" class="d-block" style="width:100%">
+<br>
+<p class="w3-serif w3-red w3-center h2">BERITA SIK</P>
+<div class="w3-container">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body action="post.php" method="post">
+
+<div class="w3-container">
+
+  <div class="w3-row">
+    <a href="javascript:void(0)" onclick="openCity(event, 'London');">
+      <div class="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">SIK mengundang dosen tamu oseanografi </div>
+    </a>
+    <a href="javascript:void(0)" onclick="openCity(event, 'Paris');">
+      <div class="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding"> MATAKA</div>
+    </a>
+  </div>
+
+  <div id="London" class="w3-container city" style="display:none">
+  <img src="tamu.jpg"><br>
+  Pada tahun 2020, terjadi perubahan besar dalam dunia pendidikan, yaitu kegiatan belajar mengajar harus dilakukan secara daring. Pandemi Covid-19 yang terjadi saat ini menuntut semua pihak untuk mau beradaptasi dengan kebiasaan-kebiasaan baru. Perkuliahan daring bukan sesuatu yang mudah bagi dosen maupun mahasiswa. Dosen dan mahasiswa harus terus belajar dan beradaptasi dengan kondisi seperti saat ini. Pada Tahun 2021 pandemi masih terus terjadi. Di Program Studi Sistem Informasi Kelautan kuliah daring dilaksanakan dengan memanfaatkan beberapa Learning management system (LMS) seperti Google Meet, Google Classroom, Zoom Cloud Meeting, maupun LMS yang dimiliki UPI yaitu SPOT.
+
+Meskipun dilaksanakan secara daring, SIK tetap berkomitmen untuk memberikan pelayanan maksimal kepada mahasiswa dalam menambah pengetahuan dan wawasan. Salah satu upaya yang dilakukan adalah menghadirkan dosen tamu. Dosen yang diundang untuk mengisi perkuliahan tamu tersebut adalah Rahmadimi Trulyan Perkasa Rohanda, S.I.K, M.T. Ia merupakan Lulusan Magister Teknik Kelautan, Institut Teknologi Bandung, dan sebagai Peneliti kelautan bidang Coastal engineer. Perkuliahan dilakukan pada setiap hari Senin jam 7.00-08.40 WIB yaitu di tanggal 11,18, dan 25 Oktober 2021.
+
+Kuliah tamu yang dilaksanan menggunakan Google Meet ini membahas tiga topik utama yaitu Karakteristik Massa Air laut, Biologi Oseanografi, dan Pasang Surut. Ia menyampaikan bahwa pada dasarnya, Oseanografi sendiri terdiri dari oseanografi Fisik, Kimia, Geologi dan Biologi. Oseanografi biologi adalah salah satu cabang utama ilmu oseanografi. Bahasan tentang oseanografi biologi mencakup:
+
+Properti yang mempengaruhi kehidupan di laut, dan perbandingan dengan kehidupan di darat;
+Gambaran fisika dan kimia laut yang meliputi cahaya, suhu, salinitas, densitas, dan tekanan yang sangat mempengaruhi kondisi dimana organisme laut hidup;
+Memperkenalkan berbagai fitoplankton, dan menjelaskan bagaimana tumbuhan ini memproduksi senyawa organik yang kaya akan energi lewat proses fotosintesis;
+Membahas tipe utama zooplankton dan pola sederhana kehidupannya serta mekanisme makan;
+Memperkenalkan aliran energi lewat rantai dan jejaring makanan di laut;
+Memperkenalkan berbagai tipe nekton termasuk Krustasea nektonik, reptil laut, mamalia laut, ikan laut, dan burung laut; dan
+Memperkenalkan tipe utama tumbuhan dan hewan yang hidup di dasar laut.
+Dengan hadirnya dosen tamu ini, menambah pengetahuan mahasiswa terkait Oseanografi dan mendapatkan gambaran mengenai penelitiian kelautan bidang Coastal engineer. [Humas SIK]
+
+</p>
+  </div>
+
+  <div id="Paris" class="w3-container city" style="display:none">
+  <img src="mataka.jpg"><br>
+  Program Studi Sistem Informasi Kelautan (SIK) dan Himpunan Mahasiswa Sistem Informasi Kelautan (HIMATASKA) setiap bulannya melaksanakan MATAKA. Pelaksanaan MATAKA ini bertujuan agar terbentuknya suasana akademik di Program Studi Sistem Informasi Kelautan.
+
+Pada bulan Oktober, MATAKA akan dilaksanakan pada tanggal 28 Oktober 2021. Adapun tema kegiatan yang diusung adalah Pemanfaatan Model Dinamika Spasial Sebagai Solusi Pengendalian Pesisie Padat Penduduk. Kegiatan ini akan di isi oleh Dosen Sistem Informasi Kelautan yaitu Della Ayu Lestari, M.Si.
+
+Kegiatan ini dikhususkan untuk mahasiswa SIK dengan maksimal peserta 100 orang dan mendapatkan free sertifikat. Kegiatan tersebut dapat disaksiakan melalui G-Meet.
+</div>
+
+
+
+<script>
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-red";
+}
+</script>
+
+
+
+
+<hr>
+<h1 class="w3-serif w3-red">INFO KAMI</h1>
+    <div class="container px-6 py-5 " id="featured-3">
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon bg-primary bg-gradient">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+        </div>
+        <h2>Hubungi kami</h2>
+          <a href="tel:(0254) 200277">Telepon : (0254) 200277</a><br>
+          <a href="mailto:sik_kdserang@upi.edu">Email :sik_kdserang@upi.edu </a><br>
+       
+      </div>
+      <div class="feature col">
+        <div class="feature-icon bg-primary bg-gradient">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
+        </div>
+        <h2>Tautan</h2>
+        <a href="http://kd-purwakarta.upi.edu/">UPI PURWAKARTA</a></li><br>
+        <a href="http://kd-serang.upi.edu/" >UPI SERANG</a></li><br>
+        <a href="http://kd-sumedang.upi.edu/" >UPI SUMEDANG</a></li><br>
+        <a href="http://kd-tasikmalaya.upi.edu/" >UPI TASIKMALAYA</a></li><br>
+        <a href="https://kd-cibiru.upi.edu/">UPI CIBIRU</a></li>
+         </div>
+      <div class="feature col">
+        <div class="feature-icon bg-primary bg-gradient">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
+        </div>
+        <div class="col-4 offset-1">
+        <form>
+      <!-- start sw-rss-feed code --> 
+<script type="text/javascript" > 
+<!-- 
+rssfeed_url = new Array(); 
+rssfeed_url[0]="https://rss.app/feeds/mXOkAF4QmA70wmds.xml";  
+rssfeed_frame_width="300"; 
+rssfeed_frame_height="210"; 
+rssfeed_scroll="on"; 
+rssfeed_scroll_step="6"; 
+rssfeed_scroll_bar="off"; 
+rssfeed_target="_blank"; 
+rssfeed_font_size="12"; 
+rssfeed_font_face=""; 
+rssfeed_border="on"; 
+rssfeed_css_url="https://feed.surfing-waves.com/css/style6.css"; 
+rssfeed_title="on"; 
+rssfeed_title_name=""; 
+rssfeed_title_bgcolor="#3366ff"; 
+rssfeed_title_color="#fff"; 
+rssfeed_title_bgimage=""; 
+rssfeed_footer="off"; 
+rssfeed_footer_name="rss feed"; 
+rssfeed_footer_bgcolor="#fff"; 
+rssfeed_footer_color="#333"; 
+rssfeed_footer_bgimage=""; 
+rssfeed_item_title_length="50"; 
+rssfeed_item_title_color="#666"; 
+rssfeed_item_bgcolor="#fff"; 
+rssfeed_item_bgimage=""; 
+rssfeed_item_border_bottom="on"; 
+rssfeed_item_source_icon="off"; 
+rssfeed_item_date="off"; 
+rssfeed_item_description="on"; 
+rssfeed_item_description_length="120"; 
+rssfeed_item_description_color="#666"; 
+rssfeed_item_description_link_color="#333"; 
+rssfeed_item_description_tag="off"; 
+rssfeed_no_items="0"; 
+rssfeed_cache = "66bd4ace5056cbd9be7ebf215bb8d9b4"; 
+//--> 
+</script> 
+<script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script> 
+<!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
+<div style="color:#ccc;font-size:10px; text-align:right; width:230px;">powered by <a href="https://surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;">Surfing Waves</a></div> 
+<!-- end sw-rss-feed code -->
+        </form>
+        
+      </div>
+    </div>  
+    <hr>
+
+<div class="d-flex justify-content-between py-4 my-4 border-top">
+  <p>© 2021 SIK UPI, Inc. All rights reserved.</p>
+</div>
+      </div>
+    </div>
+  </div>
+
+  </footer>
+</body></html>
